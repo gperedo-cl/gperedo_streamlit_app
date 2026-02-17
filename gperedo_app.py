63d6f1fd6e8c16c52aff10eb21501479a0e08296
gperedo_cl <gperedo.ici@gmail.com>
2026-02-17 15:55:58 +0100

    table mpg.csv added

--- gperedo_app.py
+++ gperedo_app.py
@@ -3,6 +3,14 @@ import streamlit as st
 # from Buttons import *
 import pandas as pd
 
-from math import ceil
+#from math import ceil
 
-st.write("Helooooo")
+#st.write("Helooooo")
+
+mpg_df = pd.read_csv("./data/mpg.csv")
+
+st.title("Streamlit App")
+
+st.table(data = mpg_df)
+
+
